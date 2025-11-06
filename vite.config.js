83e287362dd34ab4838870e929701a503a433624
Yuvarajan p <153485968+URK23CS1132@users.noreply.github.com>
2025-11-06 21:22:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/exp7/'
+  base: './', // ✅ use relative path for static deployment (like Vercel)
 })
